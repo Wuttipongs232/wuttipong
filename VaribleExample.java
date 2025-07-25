40cf9d742b1c1f3e6e619ed77e48b224a92d9dd4
Wuttipongs232 <684259055@webmail.npru.ac.th>
2025-07-25 12:07:29 +0700

    Wuttipongs232

---
+++ VaribleExample.java
@@ -0,0 +1,23 @@
+public class VaribleExample {
+    public static void main(String[] args) {
+        char clothingSize = "s"; 
+        int websiteVisitor = 0;
+        char studentGrade = "A"
+        double = studentGPA;
+        final double Pi = 3.14;
+        char text = "t";
+        boolean test = true;
+         
+         System.out.println("clothingSize")
+         System.out.println("websiteVisitor")
+         System.out.println("studentGrade")
+         System.out.println("studentGPA")
+         System.out.println("Pi")
+         System.out.println("text")
+         System.out.println("test")
+         
+            
+        );
+    }
+
+}
