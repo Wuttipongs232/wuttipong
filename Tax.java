63f69ebb25434c8418b019d17586ba2a6f51d2be
Wuttipongs232 <684259055@webmail.npru.ac.th>
2025-08-01 12:01:54 +0700

    first commit

--- Tax.java
+++ Tax.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Tax {
      public static void main (String[]args){
-      Scanner scanner = new Scanner(System.in); //Scanner ใช้เมื่อต้องการพิมพ์//
+      Scanner scanner = new Scanner(System.in); 
       System.out.print("Enter money\n");
       int money = scanner.nextInt(); //scanner.nextInt() รับข้อมูลแปลงเป็นตัวแปร//
       double sum = money*(10.0/100);
