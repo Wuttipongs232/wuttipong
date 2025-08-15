50c36deb862523b7b782fdf1bde74e47daca6697
Wuttipongs232 <684259055@webmail.npru.ac.th>
2025-08-15 21:44:03 +0700

    Newfile

--- PayDress.java
+++ PayDress.java
@@ -6,17 +6,15 @@ public class PayDress {
 
         System.out.print("Enter price per dress ");
         double PricePerDress = input.nextDouble();
-
         System.out.print("Enter number of dresses to buy ");
         int NumberOfDresses = input.nextInt();
-
         System.out.print("Enter discount rate ");
         double DiscountRate = input.nextDouble();
-
+ 
         double totalCost = PricePerDress*NumberOfDresses;
         double DiscountAmount = totalCost*DiscountRate / 100;
         double Pay = totalCost-DiscountAmount;
-
+        
         System.out.println("Total cost = "+totalCost+"Baht");
         System.out.println("DiscountAmount = "+DiscountAmount+"Baht");
         System.out.println("Pay = "+ Pay +" Baht");
